2ce77f83c9711128cf5f8137ba07766166c6c567
DataVerse2 <dataversechallenge@gmail.com>
2024-01-21 14:01:15 +0200

    forms

--- app/(dashboard)/(routes)/JoinUs/page.tsx
+++ app/(dashboard)/(routes)/JoinUs/page.tsx
@@ -6,7 +6,7 @@ const FormComponent: React.FC = () => {
       <div className="p-8 rounded shadow-md dark:shadow-lg w-3/4 border-2 black">
         <h2 className="text-2xl font-semibold mb-4">Join Us</h2>
 
-        <form action="https://formspree.io/f/mnqeaved" method="post">
+        <form action="https://formspree.io/f/mpzvqann" method="post">
 
           <label htmlFor="fname">First Name:</label>
           <input type="text" id="fname" name="firstname" placeholder="Enter your first name" className="w-full p-2 border border-gray-300 rounded my-1" required />
@@ -25,15 +25,13 @@ const FormComponent: React.FC = () => {
             pattern="[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}"
           />
 
-          <label htmlFor="subject">Subject:</label>
-          <textarea id="subject" name="subject" placeholder="Write something.." style={{ height: '200px' }} className="w-full p-2 border border-gray-300 rounded my-1" required></textarea>
+          <label htmlFor="subject">Educate, Inspire, Earn</label>
+          <textarea id="subject" name="subject" placeholder="Passionate about Teaching? Join Our Community of Tutors to Inspire Minds! If you have any questions, feel free to ask here, 
+          and we will respond as soon as possible." style={{ height: '200px' }} className="w-full p-2 border border-gray-300 rounded my-1" required></textarea>
           {/* Social Media Input */}
           <div className="mt-4">
             <label htmlFor="resume">resume:</label>
             <input type="url" id="resume" name="resume" placeholder="please insert a link for your resume" className="w-full p-2 border border-gray-200 rounded my-1" required />
-
-            <label htmlFor="github">github:</label>
-            <input type="url" id="github" name="github" placeholder="Enter your github account" className="w-full p-2 border border-gray-300 rounded my-1" />
 
             {/* Add more social media input fields as needed */}
           </div>
